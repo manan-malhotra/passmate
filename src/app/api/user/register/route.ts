752a112bb681ef5ed2ca6685e40fe1bec362dd4b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-20 10:44:06 +0530

    Register validation

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -5,6 +5,19 @@ import bcrypt, { hashSync } from "bcrypt";
 dbconnect();
 export async function POST(request: NextRequest) {
   const { username, password } = await request.json();
+  if (
+    username == null ||
+    username == "" ||
+    username.trim() == "" ||
+    password == null ||
+    password.trim() == "" ||
+    password == ""
+  ) {
+    return NextResponse.json(
+      { message: "Username or password cannot be empty" },
+      { status: 400 }
+    );
+  }
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   const user = await User.create({ username, password: hashedPassword });
