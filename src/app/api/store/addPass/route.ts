73776a88d4650f83ccf8573e5e1eb1b0d4b79fb1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-20 14:37:04 +0530

    Add password route added

---
+++ src/app/api/store/addPass/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import dbconnect from "@/db/dbconnect";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import Password from "@/models/passwordModel";
+dbconnect();
+export async function POST(request: NextRequest) {
+  const { key, username, password } = await request.json();
+  if (
+    username == null ||
+    username.trim() == "" ||
+    password == null ||
+    password.trim() == "" ||
+    key == null ||
+    key.trim() == ""
+  ) {
+    return NextResponse.json(
+      { message: "Username, Key or Password cannot be empty" },
+      { status: 400 }
+    );
+  }
+  const token = request.cookies.get("token")?.value || "";
+  if (token == "")
+    return NextResponse.json({ message: "No Token Provided" }, { status: 401 });
+  const data = jwt.decode(token) as JwtPayload;
+  const userId = data.userID;
+  const user = await User.findOne({ _id: userId });
+  if (!user)
+    return NextResponse.json(
+      { message: "Invalid Token Provided" },
+      { status: 401 }
+    );
+  const updatedKey = key.toLowerCase();
+  const alreadyExist = await Password.findOne({ user, key: updatedKey });
+  if (alreadyExist) {
+    return NextResponse.json(
+      { message: "Cannot add with same key: " + key },
+      { status: 401 }
+    );
+  }
+  const newStore = await Password.create({
+    username,
+    password,
+    user,
+    key: updatedKey,
+  });
+
+  return NextResponse.json({ message: "Key Added Successfully" });
+}
