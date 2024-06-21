720510e08a92d51af59738dd856d671ed77071a8
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-21 12:45:50 +0530

    Login page card

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="h-screen">
       <Topbar />
       <div className="flex flex-col h-5/6 items-center justify-center py-2">
-        <div className="bg-primary md:h-4/5 h-5/6 md:w-2/3 w-5/6 rounded-lg mt-auto mb-auto">
-          <p>Login</p>
+        <div className="shadow-2xl bg-primary md:h-4/5 h-5/6 md:w-2/3 w-5/6 rounded-2xl mt-auto mb-auto">
+          <p className="text-center">Login</p>
         </div>
       </div>
     </div>
