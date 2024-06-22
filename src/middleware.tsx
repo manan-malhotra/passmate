84382a3f7e22640897d789a13171271001a5d2eb
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-22 12:17:22 +0530

    Middleware logic added

---
+++ src/middleware.tsx
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const token = request.cookies.get("token")?.value || "";
+  if (path === "/") return NextResponse.redirect(new URL("/home", request.url));
+  if (token === "" && path === "/home") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  if (token !== "" && (path === "/login" || path === "/signup")) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/home", "/login", "/signup", "/"],
+};
