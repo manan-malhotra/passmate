a9df9dafd0b4980f943db5b2114e708d9dc1f2db
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-25 20:48:25 +0530

    Redirecting to home

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { useTheme } from "next-themes";
 import { MdOutlineWbSunny } from "react-icons/md";
+import { useRouter } from "next/navigation";
 export default function Topbar() {
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
   const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
   return (
     <div className="flex justify-between p-5 ">
       <div className="w-full">
         <p
-          className="ml-auto mr-auto text-center justify-center text-2xl  mt-1 tracking-widest"
-          onClick={() => {}}
+          className="ml-auto mr-auto text-center justify-center text-2xl  mt-1 tracking-widest cursor-pointer"
+          onClick={() => {
+            router.push("/");
+          }}
         >
           <span className="font-bold">PASS</span>
           <span className="font-medium">MATE</span>
